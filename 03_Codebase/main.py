fedf463e3a3b23a627cb0c5024352801fbea4915
maxjmohr <mamohoc@yahoo.com>
2024-05-06 16:54:42 +0200

    Setup source code

---
+++ 03_Codebase/main.py
@@ -0,0 +1,35 @@
+import argparse
+
+if __name__ == "__main__":
+    # Parse arguments
+    parser: argparse.ArgumentParser = argparse.ArgumentParser(
+        prog="Run Bias Experiments",
+        description="Run one of the experiments for a specific bias on one of the models.",
+        epilog="Output saved in the 'output' folder.",
+        usage="%(prog)s [options]",
+    )
+    parser.add_argument(
+        "-b",
+        "--bias",
+        type=str,
+        choices=["a", "b", "c"],
+        help="bias to study",
+        required=True,
+    )
+    parser.add_argument(
+        "-e",
+        "--experiment",
+        type=str,
+        choices=["a", "b", "c"],
+        help="experiment to run",
+        required=True,
+    )
+    parser.add_argument(
+        "-m",
+        "--model",
+        type=str,
+        choices=["gpt3.5", "gpt4", "llama2", "llama3"],
+        help="model to use",
+        required=True,
+    )
+    args: argparse.Namespace = parser.parse_args()
