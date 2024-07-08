431f242cd2f7c319a13bc2e00102de6fccc630e6
maxjmohr <mamohoc@yahoo.com>
2024-07-08 11:31:15 +0200

    Fallbacks for ollama model downloads and structured predictions

--- 03_Codebase/src/models.py
+++ 03_Codebase/src/models.py
@@ -3,8 +3,10 @@ from llama_index.core.prompts import PromptTemplate
 from llama_index.llms.ollama import Ollama
 from llama_index.llms.openai import OpenAI
 from llama_index.llms.replicate import Replicate
+import ollama
 import os
 from pydantic import BaseModel
+from pydantic_core import ValidationError
 
 
 class ExperimentOutput(BaseModel):
@@ -40,11 +42,11 @@ class ModelInteractor:
             "gpt-4o",
             "llama3",
             "llama3:70b",
-            "phi3:mini",
+            "phi3",
             "phi3:medium",
         ], f"{datetime.now()} | Model is required"
         if model not in ["gpt-3.5-turbo", "gpt-4o"] and local:
-            self.llm = self.llama(model=model, temperature=temperature)
+            self.llm = self.ollama(model=model, temperature=temperature)
         elif model in ["gpt-3.5-turbo", "gpt-4o"] and not local:
             self.llm = self.replicate(model=model)
         elif model in ["gpt-3.5-turbo", "gpt-4o"]:
@@ -53,8 +55,8 @@ class ModelInteractor:
             )
 
     @staticmethod
-    def llama(model: str = "", temperature: float = 0.7) -> Ollama:
-        """Initialize the Llama class
+    def ollama(model: str = "", temperature: float = 0.7) -> Ollama:
+        """Initialize the Ollama class
         Parameters:
         model: str
             Model to use
@@ -62,8 +64,15 @@ class ModelInteractor:
             Temperature for sampling
         Returns:
         Ollama
-            Llama class
+            Model class
         """
+        # Check if model is downloaded, else pull
+        try:
+            ollama.show(model)
+        except ollama.ResponseError:
+            ollama.pull(model)
+
+        # Initialize the model
         return Ollama(model=model, temperature=temperature)
 
     @staticmethod
@@ -115,7 +124,7 @@ class ModelInteractor:
         total_content: str,
         system_message: str = "",
         output_class: type[BaseModel] = ExperimentOutput,
-    ):
+    ) -> ExperimentOutput:
         """Get the prompt for the experiment
         Parameters:
         total_content: str
@@ -131,10 +140,49 @@ class ModelInteractor:
         assert total_content != "", f"{datetime.now()} | Experiment content is required"
 
         if system_message != "":
-            system_message = "Please answer the experiment by only giving the letter of the answer options (e.g. A, B, C, ...). Afterwards, state a short reason in 1-2 sentences for your choice. \n"
+            system_message = "Please answer the experiment by only giving the letter of the answer options (e.g. A, B, C, ...) without naming anything else! Afterwards, state a short reason in 1-2 sentences for your choice. \n"
         entire_message = system_message + "---------------------\n" + "{total_content}"
         prompt = PromptTemplate(entire_message)
 
-        return self.llm.structured_predict(
-            output_class, prompt, total_content=total_content
-        )
+        # Try the structured prediciton, sometimes it doesnt work with smaller models, then just use the completion
+        try:
+            response = self.llm.structured_predict(
+                output_class, prompt, total_content=total_content
+            )
+        except ValidationError as e:
+            print(f"Pydantic Validation Error: {e}")
+            entire_message = f"system_message---------------------\n{total_content}"
+            response = ExperimentOutput(
+                choice=str(self.llm.complete(entire_message)), reason=""
+            )
+        return response
+
+
+if __name__ == "__main__":
+    # Gemma2
+    gemma2 = ModelInteractor(model="gemma2", local=True)
+    response = gemma2.prompt(
+        "What is the capital of France? A. Paris B. London C. Berlin"
+    )
+    print(f"GEMMA2 // Choice: {response.choice}, Reason: {response.reason}")
+
+    # Llama3
+    llama3 = ModelInteractor(model="llama3", local=True)
+    response = llama3.prompt(
+        "What is the capital of France? A. Paris B. London C. Berlin"
+    )
+    print(f"LLAMA3 // Choice: {response.choice}, Reason: {response.reason}")
+
+    # Phi3
+    phi3 = ModelInteractor(model="phi3", local=True)
+    response = phi3.prompt(
+        "What is the capital of France? A. Paris B. London C. Berlin"
+    )
+    print(f"PHI3 // Choice: {response.choice}, Reason: {response.reason}")
+
+    # Phi3 Medium
+    phi3_medium = ModelInteractor(model="phi3:medium", local=True)
+    response = phi3_medium.prompt(
+        "What is the capital of France? A. Paris B. London C. Berlin"
+    )
+    print(f"PHI3 MEDIUM // Choice: {response.choice}, Reason: {response.reason}")
