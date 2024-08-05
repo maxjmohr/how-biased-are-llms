10c03f52433f800de466d2ac9e34f050fea0bd8f
maxjmohr <mamohoc@yahoo.com>
2024-08-05 17:40:41 +0200

    Slight adaption to t_responses

--- 03_Codebase/res/db_objects/t_responses.sql
+++ 03_Codebase/res/db_objects/t_responses.sql
@@ -7,12 +7,11 @@ CREATE TABLE t_responses (
     reason          VARCHAR(255) NOT NULL,
     updated_at      TIMESTAMP,
     PRIMARY KEY (experiment_id, response, updated_at),
-    FOREIGN KEY (experiment_id) REFERENCES v_experiments(experiment_id),
     FOREIGN KEY (bias_id) REFERENCES t_biases(bias_id),
     FOREIGN KEY (model_id) REFERENCES t_models(model_id)
 );
 
-COMMENT ON TABLE t_resonses IS 'Table to store the model responses to the ran experiments';
+COMMENT ON TABLE t_responses IS 'Table to store the model responses to the ran experiments';
 
 COMMENT ON COLUMN t_responses.experiment_id IS 'Unique identifier for the experiment';
 COMMENT ON COLUMN t_responses.bias_id IS 'Identifier for the bias';
