97ee8be9960245b6c7413dbc809991238646f7f3
maxjmohr <mamohoc@yahoo.com>
2024-08-07 11:40:44 +0200

    Unify backup steps into one

--- 03_Codebase/res/db_scripts/t_responses_backup.sql
+++ 03_Codebase/res/db_scripts/t_responses_backup.sql
@@ -8,16 +8,13 @@ INSERT INTO pgagent.pga_job (jobjclid, jobname, jobdesc, jobenabled, jobhostagen
 ;
 
 -- Create steps
--- Step 1: Delete old backup
-INSERT INTO pgagent.pga_jobstep (jstjobid, jstname, jstenabled, jstkind, jstdbname, jstconnstr, jstcode, jstonerror)
-    SELECT j.jobid, 'Delete old backup', true, 's', 'mthesisdb', '', 'DROP TABLE IF EXISTS t_responses_backup;', 'f'
-    FROM pgagent.pga_job j WHERE j.jobname='t_responses_backup'
-;
-
--- Step 2: Backup the responses table
-INSERT INTO pgagent.pga_jobstep (jstjobid, jstname, jstenabled, jstkind, jstdbname, jstconnstr, jstcode, jstonerror)
-    SELECT j.jobid, 'Backup the responses table', true, 's', 'mthesisdb', '', 'CREATE TABLE t_responses_backup AS TABLE t_responses;', 'f'
-    FROM pgagent.pga_job j WHERE j.jobname='t_responses_backup'
+-- Delete old backup and backup the responses table
+INSERT INTO pgagent.pga_jobstep (   jstjobid, jstname, jstenabled, jstkind, jstdbname, jstconnstr,
+                                    jstcode, jstonerror)
+    SELECT  j.jobid, 'Delete old backup and create new backup', true, 's', 'mthesisdb', '',
+            'DROP TABLE IF EXISTS t_responses_backup; CREATE TABLE t_responses_backup AS TABLE t_responses;', 'f'
+    FROM pgagent.pga_job j
+    WHERE j.jobname='t_responses_backup'
 ;
 
 -- Schedule the job to run daily at 08:45 o'clock
@@ -33,5 +30,6 @@ INSERT INTO pgagent.pga_schedule (  jscjobid, jscname, jscdesc, jscenabled, jscs
             '{t,t,t,t,t,t,t}',
             '{t,t,t,t,t,t,t,t,t,t,t,t,t,t,t,t,t,t,t,t,t,t,t,t,t,t,t,t,t,t,t,t}',
             '{t,t,t,t,t,t,t,t,t,t,t,t}'
-    FROM pgagent.pga_job j WHERE j.jobname='t_responses_backup'
+    FROM pgagent.pga_job j
+    WHERE j.jobname='t_responses_backup'
 ;
