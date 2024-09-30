dfc9728766e8d0f8da45577004386236718051e7
maxjmohr <mamohoc@yahoo.com>
2024-09-30 16:35:24 +0200

    Code // First step towards calculating binary target variable, get experiments that are ready, easy access via inquirer CLI

---
+++ 03_Codebase/res/db_objects/t_bias_detections.sql
@@ -0,0 +1,18 @@
+CREATE TABLE t_bias_detections (
+    bias            VARCHAR(255) NOT NULL,
+    scenario        VARCHAR(255) NOT NULL,
+    model           VARCHAR(255) NOT NULL,
+    temperature     VARCHAR(255) NOT NULL,
+    model_id        INTEGER NOT NULL,
+    bias_detected   NUMERIC NOT NULL,
+    updated_at      TIMESTAMP,
+    FOREIGN KEY (model_id) REFERENCES t_models(model_id)
+);
+
+COMMENT ON TABLE t_bias_detections IS 'Table to store the binary target variable bias_detected for each bias and scenario for each model';
+
+COMMENT ON COLUMN t_bias_detections.bias IS 'Name of the bias';
+COMMENT ON COLUMN t_bias_detections.scenario IS 'Scenario of the experiment';
+COMMENT ON COLUMN t_bias_detections.model_id IS 'Unique identifier for the model';
+COMMENT ON COLUMN t_bias_detections.bias_detected IS 'Binary target variable indicating whether the bias was detected';
+COMMENT ON COLUMN t_bias_detections.updated_at IS 'Timestamp of the last update';
