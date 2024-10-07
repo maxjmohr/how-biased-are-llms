1582b6f1c67d0d82b2721fd479286bf870ad3259
maxjmohr <mamohoc@yahoo.com>
2024-10-07 19:56:58 +0200

    Code // Calculation of bias_detected as effect size (Cohen's d), full implementation and some tweeks

--- 03_Codebase/res/db_objects/t_bias_detections.sql
+++ 03_Codebase/res/db_objects/t_bias_detections.sql
@@ -7,6 +7,7 @@ CREATE TABLE t_bias_detections (
     bias_detected       NUMERIC NOT NULL,
     bias_detected_mod   NUMERIC,
     updated_at          TIMESTAMP,
+    PRIMARY KEY (bias, scenario, model, temperature),
     FOREIGN KEY (model_id) REFERENCES t_models(model_id)
 );
 
@@ -14,6 +15,8 @@ COMMENT ON TABLE t_bias_detections IS 'Table to store the binary target variable
 
 COMMENT ON COLUMN t_bias_detections.bias IS 'Name of the bias';
 COMMENT ON COLUMN t_bias_detections.scenario IS 'Scenario of the experiment';
+COMMENT ON COLUMN t_bias_detections.model IS 'Name of the model';
+COMMENT ON COLUMN t_bias_detections.temperature IS 'Temperature of the experiment';
 COMMENT ON COLUMN t_bias_detections.model_id IS 'Unique identifier for the model';
 COMMENT ON COLUMN t_bias_detections.bias_detected IS 'Binary target variable indicating whether the bias was detected';
 COMMENT ON COLUMN t_bias_detections.updated_at IS 'Timestamp of the last update';
