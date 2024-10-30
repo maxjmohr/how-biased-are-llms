8ad7d4361a199e26ee88e31b83067a7d6040d973
maxjmohr <mamohoc@yahoo.com>
2024-10-30 19:12:32 +0100

    Code // Delete file

--- 03_Codebase/src/cluster/upload_data.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from src.data.db_helpers import Database
-
-db = Database()
-db.connect()
-df: pd.DataFrame = pd.read_csv("/home/tu/tu_tu/tu_zxonr37/master_thesis/03_Codebase/80160.csv")
-db.insert_data(table="t_responses", data=df, updated_at=True)
-db.disconnect()
